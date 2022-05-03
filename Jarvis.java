013600f60bd44007118306d3ffdfa98d5dd347b6
Sumit Ghag <101183868+sumitghag@users.noreply.github.com>
2022-05-03 09:03:01 +0530

    Update Jarvis.java

--- Jarvis.java
+++ Jarvis.java
@@ -5,7 +5,6 @@ public static void main (String args[])
 String a="Hi";
 String b="CDAC";
 System.out.println(a+" "+b);
-<<<<<<< HEAD
 System.out.println("Python is a high-level programming language");
 
 }
