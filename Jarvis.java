907c77894f84717c56be2e8269242c5fe977ed77
sumitghag <sumitghag95@gmail.com>
2022-05-03 09:01:18 +0530

    done

--- Jarvis.java
+++ Jarvis.java
@@ -7,9 +7,6 @@ String b="CDAC";
 System.out.println(a+" "+b);
 <<<<<<< HEAD
 System.out.println("Python is a high-level programming language");
-}}
-=======
-System.out.println("Java is an object-oriented programming language");
 
-}}
->>>>>>> b6da9622fbec0cb10d7369523d67f2116f931505
+}
+}
